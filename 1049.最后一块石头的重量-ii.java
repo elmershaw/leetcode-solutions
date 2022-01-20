85f1999c1e4ec5116ff5d999ac2cab4ef39d3dd3
elmershaw <elmershaw@163.com>
2022-01-20 21:37:04 +0800

    finished 419, 1049

--- 1049.最后一块石头的重量-ii.java
+++ 1049.最后一块石头的重量-ii.java
@@ -7,21 +7,6 @@
 // @lc code=start
 class Solution {
     public int lastStoneWeightII(int[] stones) {
-<<<<<<< HEAD
-        int sum = Arrays.stream(stones).sum();
-        int target = sum >> 1;;
-        int[] dp = new int[target + 1];
-        for (int i = 0; i < stones.length; i++) {
-            for (int j = target; j > stones[i]; j--) {
-                dp[j] = Math.max(dp[j], dp[j - stones[i]] + stones[i]);
-            }
-        }
-        for (int integer : dp) {
-            System.out.println(integer);
-        }
-        return sum - dp[target];
-=======
-        //int[] dp = new int[1501];
         int sum = Arrays.stream(stones).sum();
         int target = sum >> 1;
         int[] dp = new int[target + 1];
@@ -30,8 +15,7 @@ class Solution {
                 dp[j] = Math.max(dp[j], dp[j - stones[i]] + stones[i]);
             }
         }
-        return sum - dp[target] - dp[target];
->>>>>>> cb5f86a (add solutions from home PC)
+        return sum - (dp[target] << 1);
     }
 }
 // @lc code=end
--- 416.分割等和子集.java
+++ 416.分割等和子集.java
@@ -9,12 +9,12 @@ import java.util.Arrays;
 // @lc code=start
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        sum = Arrays.stream(nums).sum();
-        if (sum % 2 == 1) return false;
+        int n = nums.length;
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 != 0) return false;
         int target = sum >> 1;
         int[] dp = new int[target + 1];
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = target; j >= nums[i]; j--) {
                 dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
             }
