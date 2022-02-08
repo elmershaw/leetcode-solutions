32c4f57242ccabe48863cfeed4d07134b08d3117
Elmer SHAW <elmershaw@163.com>
2022-02-08 17:09:37 +0800

    finished 72

--- 583.两个字符串的删除操作.java
+++ 583.两个字符串的删除操作.java
@@ -7,19 +7,18 @@
 // @lc code=start
 class Solution {
     public int minDistance(String word1, String word2) {
-        //dp[i][j]
-        int n1 = word1.length();
-        int n2 = word2.length();
-        int[][] dp = new int[n1 + 1][n2 + 2];
-        for (int i = 1; i <= n1; i++) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
             dp[i][0] = i;
         }
-        for (int i = 1; i <= n2; i++) {
-            dp[0][i] = i;
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
         }
         dp[0][0] = 0;
-        for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
@@ -27,7 +26,7 @@ class Solution {
                 }
             }
         }
-        return dp[n1][n2];
+        return dp[m][n];
     }
 }
 // @lc code=end
--- 72.编辑距离.java
+++ 72.编辑距离.java
@@ -8,26 +8,26 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         //dp[i][j]
-        int n1 = word1.length();
-        int n2 = word2.length();
-        int[][] dp = new int[n1 + 1][n2 + 1];
-        for (int k = 1; k <= n1; k++) {
-            dp[k][0] = k;
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int [m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
         }
-        for (int k = 1; k <= n2; k++) {
-            dp[0][k] = k;
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
         }
         dp[0][0] = 0;
-        for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
                 }
             }
         }
-        return dp[n1][n2];
+        return dp[m][n];
     }
 }
 // @lc code=end
